a65b56fcc8dedb0fcb8a625a4e27021a2e010642
jsanche7 <80990132+jsanche7@users.noreply.github.com>
2021-11-03 17:29:17 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+basic.forever(function () {
+    if (input.isGesture(Gesture.Shake)) {
+        led.plot(2, 2)
+    } else {
+        led.unplot(2, 2)
+    }
+    if (input.isGesture(Gesture.TiltLeft)) {
+        led.plot(0, 2)
+    } else {
+        led.unplot(0, 2)
+    }
+    if (input.isGesture(Gesture.TiltRight)) {
+        led.plot(4, 2)
+    } else {
+        led.unplot(4, 2)
+    }
+})
